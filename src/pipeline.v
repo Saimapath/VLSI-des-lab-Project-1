02c1cac46b250a907860c648c21a991b219d0eba
Saimapath <140066295+Saimapath@users.noreply.github.com>
2026-04-07 20:58:29 +0530

    active low reset for vivado

--- src/pipeline.v
+++ src/pipeline.v
@@ -4,8 +4,8 @@ module pipe_reg #(parameter WIDTH = 32) (
     input  [WIDTH-1:0] d,
     output reg [WIDTH-1:0] q
 );
-    always @(posedge clk or posedge reset)
-        if (reset)      q <= 0;
+    always @(posedge clk or negedge reset)
+        if (!reset)      q <= 0;
         else if (clr)   q <= 0;
         else if (en)    q <= d;
         // else q <= d;
